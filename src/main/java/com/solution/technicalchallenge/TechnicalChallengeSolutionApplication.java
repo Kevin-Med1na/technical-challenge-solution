46b30df86b9f7752833b73e8751be434013824b3
Kevin David Medina <medinapedrozokevindavid@gmail.com>
2026-08-15 19:28:16 -0500

    setup proyect Technical Challenge Solution - Kevin Medina

---
+++ src/main/java/com/solution/technicalchallenge/TechnicalChallengeSolutionApplication.java
@@ -0,0 +1,13 @@
+package com.solution.technicalchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnicalChallengeSolutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnicalChallengeSolutionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/solution/technicalchallenge/TechnicalChallengeSolutionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solution.technicalchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnicalChallengeSolutionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
